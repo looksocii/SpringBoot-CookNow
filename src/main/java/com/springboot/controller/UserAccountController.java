ccf1cfdc53dcb3aac590f850d4c20a96079c2fd9
Puntakarn Kaewsanghan <aopup1122@gmail.com>
2022-09-04 15:57:13 +0700

    Update UserAccount API

--- src/main/java/com/springboot/controller/UserAccountController.java
+++ src/main/java/com/springboot/controller/UserAccountController.java
@@ -1,46 +1,52 @@
 package com.springboot.controller;
 
 import com.springboot.model.UserAccount;
-import com.springboot.repository.UserAccountRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-import java.time.LocalDateTime;
+import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
 @RequestMapping("/UserAccount")
 public class UserAccountController {
     @Autowired
-    JdbcTemplate jdbcTemplate;
+    JdbcTemplate jdbcDriver;
 
-    @Autowired
-    private UserAccountRepository userAccountRepo;
-
-    @PostMapping("/UserAccount")
-    public String listAll(Model model) {
-        String sql = "INSERT INTO useraccount (id, user_name, pass_word, email, phone, birth_date) VALUES (?, ?, ?, ?, ?, ?)";
-        int result = jdbcTemplate.update(sql, 22, "TestSB02", "12345", "test@email.com", "0801800391", LocalDateTime.now());
-
+    @PostMapping(path = "/", consumes = {"application/json"})
+    public UserAccount handleNonBrowserSubmissions(@RequestBody UserAccount userAccount) {
+        String sql = "INSERT INTO useraccount (user_name, pass_word, email, phone, birth_date) VALUES (?, ?, ?, ?, ?)";
+        int result = jdbcDriver.update(sql, userAccount.user_name, userAccount.pass_word, userAccount.email, userAccount.phone, userAccount.birth_date);
         if (result > 0) {
             System.out.println("Insert successfully.");
         }
-        return "Insert successfully.";
+        return userAccount;
     }
 
     @GetMapping(path = "/GetUsers", produces = MediaType.APPLICATION_JSON_VALUE)
     public List<UserAccount> getUserAccount()
     {
         String sql = "SELECT * FROM UserAccount";
-        List<UserAccount> listUsers = jdbcTemplate.query(sql,
-                BeanPropertyRowMapper.newInstance(UserAccount.class));
-        return listUsers;
+        return jdbcDriver.query(sql, BeanPropertyRowMapper.newInstance(UserAccount.class));
+    }
+
+    @GetMapping(path = "/{id}" , produces = MediaType.APPLICATION_JSON_VALUE)
+    public UserAccount getUser(@PathVariable(value = "id") int id)
+    {
+        String sql = "SELECT * FROM UserAccount WHERE id = "+id;
+        return jdbcDriver.queryForObject(sql, BeanPropertyRowMapper.newInstance(UserAccount.class));
+    }
+
+    @PutMapping(path = "/UpdateUser/{id}" , produces = MediaType.APPLICATION_JSON_VALUE)
+    public UserAccount updateUser(@PathVariable(value = "id") int id, @RequestBody UserAccount userAccount)
+    {
+        String sql = "UPDATE UserAccount SET user_name=?, pass_word=?, email=?, phone=?, birth_date=? WHERE id="+id;
+        int result = jdbcDriver.update(sql, userAccount.user_name, userAccount.pass_word, userAccount.email, userAccount.phone, userAccount.birth_date);
+        if (result > 0) {
+            System.out.println("Update successfully.");
+            userAccount.setId(id);
+        }
+        return userAccount;
     }
 }
--- src/main/java/com/springboot/repository/UserAccountRepository.java
+++
@@ -1,7 +0,0 @@
-package com.springboot.repository;
-
-import com.springboot.model.UserAccount;
-import org.springframework.data.jpa.repository.JpaRepository;
-public interface UserAccountRepository extends JpaRepository<UserAccount, Integer> {
-
-}
